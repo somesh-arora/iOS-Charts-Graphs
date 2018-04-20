35e7deadac8a755569001267b8377d0a606573d2
Somesh Arora <somesh.arora@mavs.uta.edu>
2018-04-20 04:21:23 -0500

    Initial Commit

---
+++ Charts&Graphs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Charts&Graphs
+//
+//  Created by Somesh Arora on 4/20/18.
+//  Copyright © 2018 Somesh Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
